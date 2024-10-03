3c58cf5e476767dc9dc92ffda5c05a57b091dc50
Ingrid-MTECH <ingridcassia0@gmail.com>
2024-10-02 21:25:42 -0300

    Add files via upload

---
+++ AULA_03/exercicio_01.py
@@ -0,0 +1,31 @@
+#Tendo como dado de entrada a altura (h) de uma pessoa, construa um programa que calcule o seu peso ideial, utilizando as seguintes fórmulas
+
+#* Para homens: (72.7*h) - 58
+#* Para mulheres: (62.1*h) - 44.7
+
+# altura1 = float(input("Insira a sua altura: "))
+# altura2 = altura1
+
+# result = (72.7 * altura1) - 58
+# result2 = (62.1 * altura2) - 44.7
+
+# print("Se você for homem, seu peso ideal é: ", result , "Se você for mulher, seu peso ideal é: ", result2)
+
+
+# Exercício para dizer o peso ideal com base na altura
+
+# altura = float(input("Qual é a sua altura? "))
+# sexo = input("Você é homem ou mulher? Escreva ""M"" para mulher e ""H"" para homem ")
+
+# H = (72.7 * altura) - 58
+# M = (62.1 * altura) - 44.7
+# if sexo == "M" or sexo == "m": #OR é uma coisa ou outra coisa, o and é os dois
+#     print(f"Seu peso ideal é:{M:.2f}")
+
+# elif sexo == "H" or sexo == "h":
+ 
+#  print(f"Seu peso ideal é:{H:.2f}")
+
+# else: 
+#     print(f"Verifique o seu sexo")
+
