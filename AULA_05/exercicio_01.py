40e0dd4ca9d5dc51f709359ad27423e367aeacad
Ingrid-MTECH <ingridcassia0@gmail.com>
2024-10-04 21:32:11 -0300

    Add files via upload

---
+++ AULA_05/exercicio_01.py
@@ -0,0 +1,60 @@
+# #Acumulador
+# soma = 0
+# for i in range(5):
+#     valor = int(input("Informe um valor: "))
+#     soma = soma + valor
+   
+
+# print(f"A soma é: {soma}") 
+
+# media = soma / (i + 1)
+# print(f"A media é: {media}")
+
+
+#Acumulador retorna o maior número
+# soma = 0
+# maior = 0
+# for i in range(5):
+#     nome = (input("Informe um nome: "))
+#     idade = int(input("Informe a idade: "))
+#     if idade > maior:
+#         maior = idade
+#         print(maior)
+#     soma = soma + idade
+    
+
+# print(f"A soma é: {soma}") 
+
+# media = soma / (i + 1)
+# print(f"A media é: {media}")
+# print(f"A maior idade é:,{maior}")
+
+#Somar idades de acordo com o sexo
+# soma = 0
+# maior = 0
+# somaf= 0
+# somam = 0
+# contf = 0
+# contm = 0
+# for i in range(3):
+#     nome = (input("Informe um nome: "))
+#     idade = int(input("Informe a idade: "))
+#     sexo = (input("Informe o seu sexo: F para feminino e M para masculino")).lower()
+#     if idade > maior:
+#         maior = idade
+                
+#     if sexo == "f":
+#         somaf = somaf + idade
+#         contf += 1
+        
+#     if sexo == "m":
+#         somam = somam + idade
+#         contm += 1  
+
+# media_m = somam / contm
+# media_f = somaf / contf
+# print(f"A soma das idades das mulheres: {somaf}") 
+# print(f"A soma das idades dos homens: {somam}") 
+# print(f"A média das idades dos homens é:{media_m}")
+# print(f"A média das idades das mulheres é:{media_f}")
+# print(f"A maior idade é:,{maior}")
